61a5b2bafa64aa601c4203305a06d3859565e8f8
Hyeonwoo Kim <clo3olb@gmail.com>
2022-03-14 18:58:15 +0800

    Created App Class.

---
+++ App.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+interface AppFunction {
+    public void execute();
+}
+
+public class App {
+    private ArrayList<AppFunction> functions = new ArrayList<AppFunction>();
+
+}
