63cd8e32fbbaa4b3565b9d104b5130877968f58c
Hyeonwoo Kim <clo3olb@gmail.com>
2022-03-17 00:15:33 +0800

    Created User.

---
+++ Ticket.java
@@ -0,0 +1,17 @@
+
+class Ticket {
+    private String ticketId;
+    private Event event;
+    private User owner;
+    private Payment payment;
+
+    Ticket() {
+
+    }
+
+    static Ticket create(Event event, User user) {
+        // creates ticket of the event and set owner to the user.
+        return null;
+    }
+
+}
